c73ca0d1edcd9bcb3d669b3f9fa5d2dd3b51f141
Tim Zhang <timzhang3@gmail.com>
2020-03-31 05:23:29 +0000

    Added animation demo using onFrame update

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const path = require('path');
 const app = express();
 
 app.use(express.static("public"));
+
+app.use('/scripts', express.static(__dirname + '/node_modules/'));
+
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
--- public/js/anim.js
+++ public/js/anim.js
@@ -1,16 +1,24 @@
 
-// Create a Paper.js Path to draw a line into it:
-var path = new Path();
-// Give the stroke a color
-path.strokeColor = 'black';
-var start = new Point(100, 100);
-// Move to start and draw a line from there
-path.moveTo(start);
-// Note the plus operator on Point objects.
-// PaperScript does that for us, and much more!
-path.lineTo(start + [ 100, -50 ]);
+spawnedShapes = [];
 
-var decagon = new Path.RegularPolygon(new Point(200, 70), 10, 50);
-decagon.fillColor = '#e9e9ff';
-decagon.selected = true;
-decagon.lineTo(start + [ 100, -50 ]);
+function onKeyDown(event) {
+    canvasSize = {
+        x: paper.view.size._width,
+        y: paper.view.size._height
+    };    
+    randomPoint = new Point(canvasSize.x * Math.random(), canvasSize.y * Math.random());
+    circle = new Path.Circle(randomPoint, 500);
+    circle.fillColor = "blue";
+    spawnedShapes.push(circle);
+}
+
+function onFrame(event) {
+	for (var i = 0; i < spawnedShapes.length; i++) {
+        spawnAnimation(spawnedShapes[i]);
+    }
+}
+
+function spawnAnimation(circle) {
+    circle.fillColor.hue += 1;
+    circle.scale(0.99);
+}
